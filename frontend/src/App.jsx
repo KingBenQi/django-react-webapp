e914220fca51c0067df8454efc1d6bef764159ea
Your Name <you@example.com>
2024-04-12 02:41:29 +0800

    on the windows

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,33 @@
 import react from "react"
 import {BrowserRouter, Routes, Route, Navigation} from "react-router-dom"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
+import ProtectedRoute from "./components/ProtectedRoute"
 
+function Logout(){
+  localStorage.clear()
+  return<Navigate to="/login" />
 
+}
+
+function RegisterAndLogout() {
+  localStorage.clear()
+  return <Register />
+}
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path = "/"
+          element
+        />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
